a574ad095556a2a12faa6309a69541236d0eeb38
Mathieu Mo <mathieumonnin@me.com>
2017-12-06 11:04:32 +0100

    #mapid dans le CSS réparé

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,19 +20,15 @@
 
 //Quand le joueur est connecté, et qu'il clique sur commencer la partie => Changement du background, apparition de la map, lancement musique.
 $(document).ready(function(){
-	$(".start").click(function(){
+	$('.start').click(function(){
 		$('.start').hide();
 		$('body').append('<style>body:before{-webkit-filter: blur(1.5px) sepia(100%) grayscale(80%) contrast(50%) opacity(50%); -moz-filter: blur(1.5px) sepia(90%) grayscale(80%) contrast(50%) opacity(50%); -o-filter: blur(1.5px) sepia(90%) grayscale(80%) contrast(50%) opacity(50%); -ms-filter: blur(1.5px) sepia(90%) grayscale(80%) contrast(50%) opacity(50%); filter: blur(1.5px) sepia(90%) grayscale(80%) contrast(50%) opacity(50%); transform: scale(1.02)!important;}</style>');
 		$('#mapid').fadeTo( 2000, 1);
 		player = document.getElementById('sound');
 		player.play();
-
 	});
-});
 
-//Quand le jouer clique sur le premier premier marker 'Commencer', le premier Popup se ferme, un second marker s'ajout et le Popup de celui-ci s'ouvre
-$(document).ready(function() {
-    $('#mapid').on('click', '#startgame', function() {
+        $('#mapid').on('click', '#startgame', function() {
          firstMarker.closePopup();
          var secondMarker = L.marker([45.7623314, 4.8275598]).addTo(map)
         .bindPopup('<h4>Là où tout devient clair</h4><p>Dans la cathédrale, j’ai trouvé un extrait de l’Évangile selon Saint-Jean. C’est un Évangile que l’on trouve également dans le Codex.<p>A l’aide du texte, tu pourras traduire:</p>').openPopup()
