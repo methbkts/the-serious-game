396c42a5cdcedb7785f6caee671990e59bab4380
Mathieu Mo <mathieumonnin@me.com>
2017-12-06 10:38:08 +0100

    Modifcation markers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,8 +20,9 @@
 
 $(document).ready(function() {
     $('#mapid').on('click', '#startgame', function() {
+         firstMarker.closePopup();
          L.marker([45.7623314, 4.8275598]).addTo(map)
-        .bindPopup('<h4>Là où tout devient clair</h4><p>Dans la cathédrale, j’ai trouvé un extrait de l’Évangile selon Saint-Jean. C’est un Évangile que l’on trouve également dans le Codex.<p>A l’aide du texte, tu pourras traduire:</p>')
+        .bindPopup('<h4>Là où tout devient clair</h4><p>Dans la cathédrale, j’ai trouvé un extrait de l’Évangile selon Saint-Jean. C’est un Évangile que l’on trouve également dans le Codex.<p>A l’aide du texte, tu pourras traduire:</p>').openPopup()
     });
 })
 
