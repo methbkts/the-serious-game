3a52cad2fadc2079d2c6cdecaf37f0e0124e8d6b
Metin Bektas <30674934+methbkts@users.noreply.github.com>
2022-12-19 14:47:13 +0100

    preupgrade commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "the-serious-game_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "the_serious_game_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
